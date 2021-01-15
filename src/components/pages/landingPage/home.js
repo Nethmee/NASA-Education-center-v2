a5f1973f3a5b42d5d78c1976746b26cf52af2c38
Nethmee <nssellahewa@gmail.com>
2021-01-15 15:10:49 +0530

    git hook testing 4

--- src/components/pages/landingPage/home.js
+++ src/components/pages/landingPage/home.js
@@ -10,7 +10,7 @@ import NewsLetter from './news letter/newsLetter';
 import Mission from './mission/mission';
 
  class home extends Component {
-     test;;;
+     let y;
     render() {
         return (
             <> 
